a464849db1b21c032f09f5bdc5ea39eb16e894b6
Ryan Rapini <ryanrapini@gmail.com>
2020-03-29 14:43:37 -0400

    laravel!

---
+++ resources/js/components/brackets.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-sm" v-if="true">
+            {{ $store.state.data }}
+        </div>
+        <div class="col-sm">
+            <div class="form-group">
+                <label for="data-box">Data (json)</label>
+                <textarea class="form-control" id="data-box" rows="3"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            
+        }
+    }
+</script>
---
+++ resources/js/components/data-input.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-sm">
+            <div class="form-group">
+                <label for="data-box">Data (json)</label>
+                <textarea class="form-control" id="data-box" rows="3" v-model="input" @input="applyData"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                input: ''
+            }
+        },
+        methods: {
+            applyData: function() {
+                this.$store.commit('apply', this.input);
+            }
+        }
+    }
+</script>
